42140a565b137c49b83a1aeace644bf11e4b1d2e
ayuayue <1401262639@qq.com>
2020-06-15 22:40:56 +0800

    第五天 分布式节点

---
+++ gocache/peers.go
@@ -0,0 +1,13 @@
+package gocache
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+
+type PeerPicker interface {
+	PickPeer(key string) (peer PeeGetterr, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer
+type PeerGetter interface {
+	Get(group string, key string) ([]byte, error)
+}
